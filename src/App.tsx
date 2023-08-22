3de37af593eadf9ad171dc205b76ad7ce1852865
Foxygene <foxygene.dev@gmail.com>
2023-08-22 06:51:52 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000 * 60);
+
+    return () => clearInterval(interval);
+  }, [date, setDate]);
+
+  const dateString = date.toLocaleString("en-US", {
+    timeZone: "CET",
+    weekday: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return (
+    <div className="flex space-between">
+      <div>CH 13</div>
+      <div>{dateString}</div>
+    </div>
+  );
+}
+
+export default App;
